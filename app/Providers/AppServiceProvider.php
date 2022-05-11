e2cf5ea09d989329c08272583a7baeb129034d74
Carlos Mora <cam@people-experts.com>
2022-05-11 12:12:30 +0200

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Blueprint::macro('userstamps', function () {
+            $this->foreignId('created_by')->nullable();
+            $this->foreignId('updated_by')->nullable();
+            $this->foreignId('deleted_by')->nullable();
+        });
+    }
+}
